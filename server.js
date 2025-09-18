ef78e0ea65ada7813e089f1c4119727b409705d8
JanDalhuysen <dalhuysen.j@gmail.com>
2025-09-18 12:11:36 +0200

    fix: change port from 3000 to 5000

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 http.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
